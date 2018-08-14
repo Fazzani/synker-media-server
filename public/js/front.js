66cae7e2cf744815fe6764d45dcf4cb1d742cf0e
Fazzani <tunisienheni@outlook.com>
2018-08-14 22:41:49 +0200

    fix

--- public/js/front.js
+++ public/js/front.js
@@ -1,3 +1,5 @@
+
+
 $(function () {
 
     // ------------------------------------------------------- //
@@ -10,25 +12,25 @@ $(function () {
     // Universal Form Validation
     // ------------------------------------------------------ //
 
-    $('.form-validate').each(function() {  
-        $(this).validate({
-            errorElement: "div",
-            errorClass: 'is-invalid',
-            validClass: 'is-valid',
-            ignore: ':hidden:not(.summernote),.note-editable.card-block',
-            errorPlacement: function (error, element) {
-                // Add the `invalid-feedback` class to the error element
-                error.addClass("invalid-feedback");
-                //console.log(element);
-                if (element.prop("type") === "checkbox") {
-                    error.insertAfter(element.siblings("label"));
-                } 
-                else {
-                    error.insertAfter(element);
-                }
-            }
-        });
-    });
+    // $('.form-validate').each(function() {  
+    //     $(this).validate({
+    //         errorElement: "div",
+    //         errorClass: 'is-invalid',
+    //         validClass: 'is-valid',
+    //         ignore: ':hidden:not(.summernote),.note-editable.card-block',
+    //         errorPlacement: function (error, element) {
+    //             // Add the `invalid-feedback` class to the error element
+    //             error.addClass("invalid-feedback");
+    //             //console.log(element);
+    //             if (element.prop("type") === "checkbox") {
+    //                 error.insertAfter(element.siblings("label"));
+    //             } 
+    //             else {
+    //                 error.insertAfter(element);
+    //             }
+    //         }
+    //     });
+    // });
 
     // ------------------------------------------------------- //
     // Material Inputs
